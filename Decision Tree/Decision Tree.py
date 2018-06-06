a7272633f590856d5c5e15a7a5bcfdd249b7d607
m2lgsb <martin2lgsb@gmail.com>
2018-06-06 16:55:18 +0800

    decision tree

---
+++ Decision Tree/Decision Tree.py
@@ -0,0 +1,135 @@
+# -*- coding: UTF-8 -*-
+from math import log
+import operator
+import pickle
+
+
+def calcShannonEnt(dataSet):
+    numEntires = len(dataSet)                        #返回数据集的行数
+    labelCounts = {}                                #保存每个标签(Label)出现次数的字典
+    for featVec in dataSet:                            #对每组特征向量进行统计
+        currentLabel = featVec[-1]                    #提取标签(Label)信息
+        if currentLabel not in labelCounts.keys():    #如果标签(Label)没有放入统计次数的字典,添加进去
+            labelCounts[currentLabel] = 0
+        labelCounts[currentLabel] += 1                #Label计数
+    shannonEnt = 0.0                                #经验熵(香农熵)
+    for key in labelCounts:                            #计算香农熵
+        prob = float(labelCounts[key]) / numEntires    #选择该标签(Label)的概率
+        shannonEnt -= prob * log(prob, 2)            #利用公式计算
+    return shannonEnt                                #返回经验熵(香农熵)
+
+
+def createDataSet():
+    dataSet = [[0, 0, 0, 0, 'no'],                        #数据集
+               [0, 0, 0, 1, 'no'],
+               [0, 1, 0, 1, 'yes'],
+               [0, 1, 1, 0, 'yes'],
+               [0, 0, 0, 0, 'no'],
+               [1, 0, 0, 0, 'no'],
+               [1, 0, 0, 1, 'no'],
+               [1, 1, 1, 1, 'yes'],
+               [1, 0, 1, 2, 'yes'],
+               [1, 0, 1, 2, 'yes'],
+               [2, 0, 1, 2, 'yes'],
+               [2, 0, 1, 1, 'yes'],
+               [2, 1, 0, 1, 'yes'],
+               [2, 1, 0, 2, 'yes'],
+               [2, 0, 0, 0, 'no']]
+    labels = ['年龄', '有工作', '有自己的房子', '信贷情况']        #特征标签
+    return dataSet, labels                             #返回数据集和分类属性
+
+
+def splitDataSet(dataSet, axis, value):
+    retDataSet = []                                        #创建返回的数据集列表
+    for featVec in dataSet:                             #遍历数据集
+        if featVec[axis] == value:
+            reducedFeatVec = featVec[:axis]                #去掉axis特征
+            reducedFeatVec.extend(featVec[axis+1:])     #将符合条件的添加到返回的数据集
+            retDataSet.append(reducedFeatVec)
+    return retDataSet                                      #返回划分后的数据集
+
+
+def chooseBestFeatureToSplit(dataSet):
+    numFeatures = len(dataSet[0]) - 1                    #特征数量
+    baseEntropy = calcShannonEnt(dataSet)                 #计算数据集的香农熵
+    bestInfoGain = 0.0                                  #信息增益
+    bestFeature = -1                                    #最优特征的索引值
+    for i in range(numFeatures):                         #遍历所有特征
+        #获取dataSet的第i个所有特征
+        featList = [example[i] for example in dataSet]
+        uniqueVals = set(featList)                         #创建set集合{},元素不可重复
+        newEntropy = 0.0                                  #经验条件熵
+        for value in uniqueVals:                         #计算信息增益
+            subDataSet = splitDataSet(dataSet, i, value)         #subDataSet划分后的子集
+            prob = len(subDataSet) / float(len(dataSet))           #计算子集的概率
+            newEntropy += prob * calcShannonEnt(subDataSet)     #根据公式计算经验条件熵
+        infoGain = baseEntropy - newEntropy                     #信息增益
+        # print("第%d个特征的增益为%.3f" % (i, infoGain))            #打印每个特征的信息增益
+        if (infoGain > bestInfoGain):                             #计算信息增益
+            bestInfoGain = infoGain                             #更新信息增益，找到最大的信息增益
+            bestFeature = i                                     #记录信息增益最大的特征的索引值
+    return bestFeature                                             #返回信息增益最大的特征的索引值
+
+
+
+def majorityCnt(classList):
+    classCount = {}
+    for vote in classList:                                        #统计classList中每个元素出现的次数
+        if vote not in classCount.keys():classCount[vote] = 0
+        classCount[vote] += 1
+    sortedClassCount = sorted(classCount.items(), key = operator.itemgetter(1), reverse = True)        #根据字典的值降序排序
+    return sortedClassCount[0][0]                                #返回classList中出现次数最多的元素
+
+
+def createTree(dataSet, labels, featLabels):
+    classList = [example[-1] for example in dataSet]            #取分类标签(是否放贷:yes or no)
+    if classList.count(classList[0]) == len(classList):            #如果类别完全相同则停止继续划分
+        return classList[0]
+    if len(dataSet[0]) == 1:                                    #遍历完所有特征时返回出现次数最多的类标签
+        return majorityCnt(classList)
+    bestFeat = chooseBestFeatureToSplit(dataSet)                #选择最优特征
+    bestFeatLabel = labels[bestFeat]                            #最优特征的标签
+    featLabels.append(bestFeatLabel)
+    myTree = {bestFeatLabel:{}}                                    #根据最优特征的标签生成树
+    del(labels[bestFeat])                                        #删除已经使用特征标签
+    featValues = [example[bestFeat] for example in dataSet]        #得到训练集中所有最优特征的属性值
+    uniqueVals = set(featValues)                                #去掉重复的属性值
+    for value in uniqueVals:                                    #遍历特征，创建决策树。
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value), labels, featLabels)
+    return myTree
+
+
+def classify(inputTree, featLabels, testVec):
+    firstStr = next(iter(inputTree))                                                        #获取决策树结点
+    secondDict = inputTree[firstStr]                                                        #下一个字典
+    featIndex = featLabels.index(firstStr)
+    for key in secondDict.keys():
+        if testVec[featIndex] == key:
+            if type(secondDict[key]).__name__ == 'dict':
+                classLabel = classify(secondDict[key], featLabels, testVec)
+            else: classLabel = secondDict[key]
+    return classLabel
+
+
+def storeTree(inputTree, filename):
+    with open(filename, 'wb') as fw:
+        pickle.dump(inputTree, fw)
+
+
+def grabTree(filename):
+    fr = open(filename, 'rb')
+    return pickle.load(fr)
+
+if __name__ == '__main__':
+    dataSet, labels = createDataSet()
+    featLabels = []
+    myTree = createTree(dataSet, labels, featLabels)
+    testVec = [0,1]                                        #测试数据
+    result = classify(myTree, featLabels, testVec)
+    print(result)
+
+    # myTree = {'有自己的房子': {0: {'有工作': {0: 'no', 1: 'yes'}}, 1: 'yes'}}
+    # storeTree(myTree, 'classifierStorage.txt')
+
+    myTree = grabTree('classifierStorage.txt')
+    print(myTree)
---
+++ Decision Tree/Sklearn-Decision Tree.py
@@ -0,0 +1,42 @@
+# -*- coding: UTF-8 -*-
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.externals.six import StringIO
+from sklearn import tree
+import pandas as pd
+import numpy as np
+import pydotplus
+
+if __name__ == '__main__':
+    with open('lenses.txt', 'r') as fr:
+        lenses = [inst.strip().split('\t') for inst in fr.readlines()]
+    lenses_target = []
+    for each in lenses:
+        lenses_target.append(each[-1])
+    print(lenses_target)
+
+    lensesLabels = ['age', 'prescript', 'astigmatic', 'tearRate']
+    lenses_list = []
+    lenses_dict = {}
+    for each_label in lensesLabels:
+        for each in lenses:
+            lenses_list.append(each[lensesLabels.index(each_label)])
+        lenses_dict[each_label] = lenses_list
+        lenses_list = []
+    # print(lenses_dict)
+    lenses_pd = pd.DataFrame(lenses_dict)
+    # print(lenses_pd)
+    le = LabelEncoder()
+    for col in lenses_pd.columns:
+        lenses_pd[col] = le.fit_transform(lenses_pd[col])
+    # print(lenses_pd)
+
+    clf = tree.DecisionTreeClassifier(max_depth = 4)
+    clf = clf.fit(lenses_pd.values.tolist(), lenses_target)
+    dot_data = StringIO()
+    tree.export_graphviz(clf, out_file = dot_data,
+                         feature_names = lenses_pd.keys(),
+                         class_names = clf.classes_,
+                         filled=True, rounded=True,
+                         special_characters=True)
+    graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+    graph.write_pdf("tree.pdf")
