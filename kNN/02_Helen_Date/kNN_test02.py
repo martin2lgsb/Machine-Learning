da0f97a61fccc84cba684e36a55c0cb9864b6c01
m2lgsb <martin2lgsb@gmail.com>
2018-06-06 14:46:01 +0800

    fix bug and prapre_for hand-written num

--- kNN/02_Helen_Date/kNN_test02.py
+++ kNN/02_Helen_Date/kNN_test02.py
@@ -30,7 +30,7 @@ def file2matrix(filename):
     for line in arryOLines:
         line = line.strip()
         listFromLine = line.split('\t')
-        returnMat[index, :] = listFromLine[0 : 3]
+        returnMat[index, :] = listFromLine[0: 3]
         if listFromLine[-1] == 'didntLike':
             classLableVector.append(1)
         elif listFromLine[-1] == 'smallDoses':
@@ -39,6 +39,7 @@ def file2matrix(filename):
             classLableVector.append(3)
         index += 1
     return returnMat, classLableVector
+
 
 def autoNorm(dataSet):
     minVals = dataSet.min(0)
---
+++ kNN/03_Handwritten_Number/kNN_test03.py
@@ -0,0 +1,3 @@
+# -*- coding: UTF-8 -*-
+
+# todo kNN - 手写数字识别 hand-written numeral recognition
